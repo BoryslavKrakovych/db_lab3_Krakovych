ca75cde7e8880f78f6ba6c41810903a9bf5c1d1a
BoryslavKrakovych <91006670+BoryslavKrakovych@users.noreply.github.com>
2023-11-03 22:29:25 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,11 +1,10 @@
---1. Результати тесту з математики кожного зі студентів
-select student_id, score
-from results
-where test_id = '1'
---2. Рівень освіти тих студентів, якиі написали тест з читання від 60 балів і вище
-select student_id
-from results
-group by level
---3. Кількість студенток, що писали тест на рівень магістра
-select count(distinct(results.student_id)) from student,results
-where gender='female' and results.level_id='3'
+--1. Вивести рівень освіти та кількість студенток, що писали тести на диплом цього рівня 
+select level_id, count(distinct(results.student_id)) from student,results
+where results.student_id in(select student_id from student where gender='female') group by level_id  
+
+--2. Вивести етнічні групи і кількість студентів та сутденток, які належать до кожної з етнічних груп
+select ethnicity, count(student_id) from student group by ethnicity
+
+--3. Вивести id студентів-чоловіків, та їх бали за тест з математики
+select student_id, score from results
+where test_id = '1' and student_id in(select student_id from student where gender='male')
