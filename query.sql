bad2ec775ca4fea93300b7b1ae713679f860c03a
BoryslavKrakovych <91006670+BoryslavKrakovych@users.noreply.github.com>
2023-11-06 12:10:58 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -2,9 +2,12 @@
 select level_id, count(distinct(results.student_id)) from student,results
 where results.student_id in(select student_id from student where gender='female') group by level_id  
 
---2. Вивести етнічні групи і кількість студентів та сутденток, які належать до кожної з етнічних груп
-select ethnicity, count(student_id) from student group by ethnicity
+--2. Вивести кількість студентів-чоловіків, що написали тест з математики більше на 60 балів
+select distinct (results.student_id) from student,results 
+where results.student_id in(select student_id from student where gender='male') and test_id=1 and score >= 60  
 
---3. Вивести id студентів-чоловіків, та їх бали за тест з математики
-select student_id, score from results
-where test_id = '1' and student_id in(select student_id from student where gender='male')
+--3. Вивести id студенток етнічної групи B, що написали тест з письма на диплом середньої школи менше 60 балів
+select distinct (results.student_id) from student,results 
+where results.student_id in(select student_id from student where gender='female' and ethnicity='group B') 
+and test_id=3 and level_id=5 and score < 60  
+
